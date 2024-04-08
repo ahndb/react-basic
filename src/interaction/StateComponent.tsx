63fbc46ee7b081d61504aa0d19e0de843b716db5
ahndb <asi0000@nate.com>
2024-04-08 12:04:22 +0900

    240408 11:50 Update StateComponent

--- src/interaction/StateComponent.tsx
+++ src/interaction/StateComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 
 // 상태(status)
 // - 각각의 컴포넌트가 가지는 데이터
@@ -16,7 +16,11 @@ export default function StateComponent() {
   const [count, setCount] = useState<number>(0);
   // let total: number = 0;
   const [total, settotal] = useState<number>(0);
+  // let favorite: string [] = [];
+  const [favorites, setFavorites] = useState<string[]>(['사과']);
   
+  const [favorite, setFavorite] = useState<string>('');
+
   const onCountAddHandler = () => {
     //! 상태 변수는 반드시 상태변경함수로 변경을 해야 리렌더링됨
 
@@ -54,11 +58,42 @@ export default function StateComponent() {
 
   };
 
+  //? Input 요소의 값을 가져오고자 할땐 onChange 이벤트의 .target.value로 가져옴
+  const onInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    // 가져온 .target.value 값을 상태에 지정
+    setFavorite(event.target.value);
+  };
+
+  const onAddListHandler = () => {
+    //? 상태가 배열 혹은 객체이면 각각에 대해 요소를 추가, 변경이 일어나도 상태가 변경된 것으로 인식하지 않음(실제 주소 변경이 일어나지 않아서..)
+    // 상태가 변경된 것으로 인식하게 하고 싶으면 새로운 배열 혹은 객체를 생성하여 상태를 변경해야 함 (일반적으로 복사해서 변경)
+    //* 함수 안에서 새 배열을 만들어서 넣어주기
+    // favorites.push(favorite);
+    //* const newFavorites = favorites.map(item => item)
+    // console.log(newFavorites);
+    // setFavorites(newFavorites)
+
+    setFavorites([...favorites, favorite]); //기존에 것을 스프레드 시키고 favorite으로 추가되는 애들 넣기(그냥 복사할 때)
+    // ['사과', '바나나', '포도'] 참조형태가 아니라서 없어지지는 않음
+    setFavorite('');
+  };
+
   return (
     <>
       <button onClick={onCountAddHandler}>+</button>
       <h1>{count}</h1>
       <h1>{total}</h1>
+
+      <hr />
+      
+      {/* value값을 맞추면 input창과 h4에 값이 같이지며 ''이 됨, 
+      Input이 만약 상태를 변경한다면 value로 그 상태를 지정해야 불일치가 발생하지 않음 */}
+      <input onChange={onInputHandler} type="text" value={favorite} /> 
+      <button onClick={onAddListHandler}>추가</button>
+      <h4>{favorite}</h4>
+      <ul>
+        {favorites.map((item, index) => <li key={index}>{item}</li>)}
+      </ul>
     </>
   )
 }
